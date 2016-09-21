2274fc4d38903b57f4cbec54d0df6af72dfe3afd
webgua <shenxvdong1@gmail.com>
2016-09-21 21:09:15 +0800

    solve chinese problemm

--- trees/readFile.py
+++ trees/readFile.py
@@ -9,31 +9,31 @@ reload(sys)
 sys.setdefaultencoding( "utf-8" )
 
 
-# file = codecs.open('003.txt','r');
+file = codecs.open('003.txt','r');
 
-# text =  file.read().decode('gbk')
+text =  file.read().decode('gbk')
 
-# seg_list = jieba.cut(text, cut_all=False)
+seg_list = jieba.cut(text, cut_all=False)
 
 
-# wordCounts = {}
+wordCounts = {}
 
-# for words in seg_list:
-# 	currentLabel = words
-# 	if currentLabel not in wordCounts.keys():
-# 		wordCounts[currentLabel] = 0
-# 	wordCounts[currentLabel]+=1
+for words in seg_list:
+	currentLabel = words
+	if currentLabel not in wordCounts.keys():
+		wordCounts[currentLabel] = 0
+	wordCounts[currentLabel]+=1
 
-# jsonlist = []
+jsonlist = []
 
-# # dict= sorted(wordCounts.iteritems(), key=lambda d:d[1], reverse = True)
-# for key in wordCounts.keys():
-# 	if wordCounts[key] > 50:
-# 		# info = {}
-# 		# info["key"] = key
-# 		# info["counts"] = wordCounts[key]
-# 		info=(key,wordCounts[key])
-# 		jsonlist.append(info)
+# dict= sorted(wordCounts.iteritems(), key=lambda d:d[1], reverse = True)
+for key in wordCounts.keys():
+	if wordCounts[key] > 150 & len(key)>=2:
+		# info = {}
+		# info["key"] = key
+		# info["counts"] = wordCounts[key]
+		info=(key,wordCounts[key])
+		jsonlist.append(info)
 
 # savefile = open('jueji.json','w')
 # savefile.write("")
@@ -48,16 +48,16 @@ sys.setdefaultencoding( "utf-8" )
 # 	file2.write('\n')
 
 # file2.close()
-jsonlist2 = [("的是",4),("有趣",2)]
+# jsonlist2 = [(u"的是",4),(u"有趣",2)]
 
-wordcloud = WordCloud().fit_words(jsonlist2)
+wordcloud = WordCloud(font_path="C:\Windows\Fonts\Microsoft YaHei UI\msyh.ttc").fit_words(jsonlist)
 
 import matplotlib.pyplot as plt 
 plt.imshow(wordcloud) 
 plt.axis("off") 
 # take relative word frequencies into account, lower max_font_size 
 #wordcloud = WordCloud(max_font_size=40, relative_scaling=.5).generate(text) 
-wordcloud = WordCloud(max_font_size=40, relative_scaling=.5).fit_words(jsonlist2) 
+wordcloud = WordCloud(max_font_size=40, relative_scaling=.5,font_path="C:\Windows\Fonts\Microsoft YaHei UI\msyh.ttc").fit_words(jsonlist) 
 plt.figure() 
 plt.imshow(wordcloud) 
 plt.axis("off") 
